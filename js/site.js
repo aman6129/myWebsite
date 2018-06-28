e5cca5cd2f743b1a18435d25079c1bc463598f51
Mohammad Anees <mohammadanees@Mohammads-MBP.lan1>
2018-06-27 22:58:03 -0500

    Update script to handle scroll/resize . Add more content

--- js/site.js
+++ js/site.js
@@ -1,17 +1,62 @@
+// Mohammad Anees
+// 6/27/2018
 
 var controls = document.getElementById('controls');
-// console.log(controls);
+var controlsHeight = controls.getBoundingClientRect().height;
+var windowHeight = window.innerHeight - controlsHeight;
+var ticking = false;
 
-controls.addEventListener('click', function(e) {
-    console.log(controls.getBoundingClientRect());
-    var yPosition = controls.getBoundingClientRect().y;
-    if(yPosition <= 0){
+function toggleControlsState(){
+    var yPosition = window.scrollY;
+    var isLocked = (windowHeight - yPosition) <= 0 ? true : false;
+    var classList = controls.classList.value.split(' ');
+    var isStateAbsolute = classList.indexOf('absolute') !== -1 ? true : false;
+    var isStateFixed = classList.indexOf('fixed') !== -1 ? true : false;
+
+    if(isLocked && isStateAbsolute){
         controls.classList.remove('absolute');
         controls.classList.add('fixed');
     }
-    else{
+    else if(!isLocked && isStateFixed){
         controls.classList.add('absolute');
         controls.classList.remove('fixed');
     }
-    
-  }, false);
+}
+
+window.addEventListener('scroll', function(e) {
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+          toggleControlsState();
+          ticking = false;
+        });
+         
+        ticking = true;
+    }
+}, false);
+
+
+// handle window resize for controls container efficiently
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function() {
+    windowHeight = window.innerHeight - controlsHeight;
+    toggleControlsState();
+});
