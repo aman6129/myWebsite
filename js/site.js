9fdba012e1d99ec826ed1fd26f2df1510daea0dc
Mohammad Anees <mohammadanees@mohammads-mbp.attlocal.net>
2018-07-04 19:52:52 -0500

    update scrolling controls to change color based on active div

--- js/site.js
+++ js/site.js
@@ -2,12 +2,50 @@
 // 6/27/2018
 
 var controls = document.getElementById('controls');
+var contentDivs = document.getElementsByClassName('content');
+var activeDiv = contentDivs[0];
+var divBoundings = createDivBoundings();
 var controlsHeight = controls.getBoundingClientRect().height;
 var windowHeight = window.innerHeight - controlsHeight;
 var ticking = false;
 
+function createDivBoundings(){
+    doc = document.getElementsByTagName('html')[0];
+    var divBoundings = [];
+    var classColorList = {
+        'about-container content' : { backgroundColor: '#222222', textColor: 'white' } ,
+        'projects-container content' : { backgroundColor: '#FFED00', textColor: '#222222' }
+    }
+    for(var i = 0; i < contentDivs.length; ++i){
+        contentDiv = contentDivs[i];
+        var divTopLimit = contentDiv.scrollHeight;
+        var divBottomLimit = divTopLimit + window.innerHeight - 50;
+        var className = contentDiv.classList.toString();
+
+        divBoundings.push({
+            name: className,
+            backgroundColor: classColorList[className].backgroundColor,
+            textColor: classColorList[className].textColor,
+            target: contentDiv,
+            scrollHeight: contentDiv.scrollHeight,
+            topLimit: divTopLimit,
+            bottomLimit: divBottomLimit
+        });
+    }
+    console.log(divBoundings);
+    return divBoundings;
+}
+
+function getCurrentDiv(yPos){
+    var currentDiv = divBoundings.filter(function(bounding){
+        return bounding.topLimit <= yPos && bounding.bottomLimit > yPos;
+    })[0];
+
+    return currentDiv;
+}
+
 function toggleControlsState(){
-    var yPosition = window.scrollY;
+    var yPosition = window.pageYOffset;
     var isLocked = (windowHeight - yPosition) <= 0 ? true : false;
     var classList = controls.classList.value.split(' ');
     var isStateAbsolute = classList.indexOf('absolute') !== -1 ? true : false;
@@ -23,10 +61,24 @@ function toggleControlsState(){
     }
 }
 
+function adjustControlsColors(){
+    var currentDiv = getCurrentDiv(window.pageYOffset);
+    if(currentDiv !== undefined){
+        if(currentDiv.name !== activeDiv.classList.toString()){
+            divBackgroundColor = currentDiv.backgroundColor;
+            divTextColor = currentDiv.textColor;
+            controls.style.backgroundColor = divBackgroundColor;
+            controls.style.color = divTextColor;
+            activeDiv = currentDiv.target;
+        }
+    }
+}
+
 window.addEventListener('scroll', function(e) {
     if (!ticking) {
         window.requestAnimationFrame(function() {
           toggleControlsState();
+          adjustControlsColors();
           ticking = false;
         });
          
